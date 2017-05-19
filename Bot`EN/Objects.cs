e0383b9399434ee48284ab8260ab58706b385a00
Sergey Barabanov <barabanov.mail@gmail.com>
2017-05-19 15:16:20 +0300

    first version

---
+++ Bot`EN/Objects.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Bot_EN
+{
+    enum Type
+    {
+        Empty = 0,
+        Add = 1,
+        New = 2,
+        Tng = 3,
+    }
+
+    class Word
+    {
+        public string EN { get; set; }
+        public string RU { get; set; }
+        public Description Ins { get; set; }
+        public DateTime Prev { get; set; }
+        public DateTime Next { get; set; }
+
+        public Word()
+        {
+
+        }
+
+        public Word(string en)
+        {
+            EN = en;
+            RU = GetTranslation(en);
+            Ins = GetDescription(en);
+            Prev = DateTime.Now;
+            Next = DateTime.MinValue;
+        }
+
+        private string GetTranslation(string text)
+        {
+            WebResponse response = WebRequest.Create(string.Format("https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170516T081423Z.897ef08ab302ee33.26081f61eea6100b6d4083ecddf8679b004965dd&lang=en-ru&text={0}", text)).GetResponse();
+            StreamReader stream = new StreamReader(response.GetResponseStream());
+
+            string[] array = JsonConvert.DeserializeObject<string[]>(JObject.Parse(stream.ReadToEnd())["text"].ToString());
+            stream.Close();
+
+            return array.Last();
+        }
+
+        private Description GetDescription(string text)
+        {
+            WebResponse response = WebRequest.Create(string.Format("https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=dict.1.1.20170516T091153Z.526c208fb316a52d.71ad1af0b2c524499453f469a44f4612802af1f0&lang=en-ru&text={0}&flags=4", text)).GetResponse();
+            StreamReader stream = new StreamReader(response.GetResponseStream());
+
+            Description ins = JsonConvert.DeserializeObject<Description>(stream.ReadToEnd());
+            stream.Close();
+
+            return ins;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Word;
+            if (other == null)
+                return false;
+
+            return EN == other.EN && RU == other.RU;
+        }
+
+        public override int GetHashCode()
+        {
+            return string.Format("{0}#{1}", EN, RU).GetHashCode();
+        }
+    }
+
+    // yandex object`s
+
+    public class Txt
+    {
+        public string Text { get; set; }
+    }
+
+    public class Example
+    {
+        public string Text { get; set; }
+        public Txt[] Tr { get; set; }
+    }
+
+    public class Translation
+    {
+        public string Text { get; set; }
+        public string Pos { get; set; }
+        public Txt[] Syn { get; set; }
+        public Txt[] Mean { get; set; }
+        public Example[] Ex { get; set; }
+    }
+
+    public class Definition
+    {
+        public string Text { get; set; }
+        public string Pos { get; set; }
+        public string Ts { get; set; }
+        public Translation[] Tr { get; set; }
+    }
+
+    public class Description
+    {
+        public Definition[] Def { get; set; }
+    }
+
+    // telegram object`s
+
+    public class User
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+    }
+
+    public class Chat
+    {
+        public int id { get; set; }
+        public string type { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+    }
+
+    public class Message
+    {
+        public int message_id { get; set; }
+        public User from { get; set; }
+        public Chat chat { get; set; }
+        public int date { get; set; }
+        public string text { get; set; }
+    }
+
+    public class CallbackQuery
+    {
+        public string id { get; set; }
+        public User from { get; set; }
+        public Message message { get; set; }
+        public string inline_message_id { get; set; }
+        public string data { get; set; }
+    }
+
+    public class Update
+    {
+        public int update_id { get; set; }
+        public Message message { get; set; }
+        public CallbackQuery callback_query { get; set; }
+    }
+
+    public class InlineKeyboardMarkup
+    {
+        public InlineKeyboardButton[][] inline_keyboard { get; set; }
+    }
+
+    public class InlineKeyboardButton
+    {
+        public string text { get; set; }
+        public string callback_data { get; set; }
+    }
+}
---
+++ Bot`EN/Program.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Bot_EN
+{
+    class Program
+    {
+        static string token = "330426260:AAGOVMlLotr2_bn_PwfWWacyZtgObEULFvA";
+
+        static void Main()
+        {
+            List<Word> words = JsonConvert.DeserializeObject<Word[]>(File.ReadAllText("word`s")).ToList();
+
+            new Thread(t => {
+                while (true)
+                {
+                    DateTime now = DateTime.Now;
+
+                    File.WriteAllText("word`s", JsonConvert.SerializeObject(words.OrderBy(w => w.EN)));
+                    Console.WriteLine("write {0} word`s in file", words.Count);
+
+                    while ((DateTime.Now - now).TotalMinutes < 1)
+                        Thread.Sleep(5000);
+                }
+            }).Start();
+
+            InlineKeyboardMarkup keyboardEmpty = new InlineKeyboardMarkup {
+                inline_keyboard = new InlineKeyboardButton[][] {
+                    new InlineKeyboardButton[] {
+                        new InlineKeyboardButton { text = "add word`s", callback_data = "add" },
+                        new InlineKeyboardButton { text = "new word`s", callback_data = "new" },
+                        new InlineKeyboardButton { text = "tng word`s", callback_data = "tng" },
+                    },
+                }
+            };
+
+            InlineKeyboardMarkup keyboardNew = new InlineKeyboardMarkup {
+                inline_keyboard = new InlineKeyboardButton[][] {
+                    new InlineKeyboardButton[] {
+                        new InlineKeyboardButton { text = "save", callback_data = "save" },
+                        new InlineKeyboardButton { text = "next", callback_data = "next" },
+                    },
+                }
+            };
+
+            Update update = GetUpdate();
+
+            while (update == null)
+            {
+                Console.WriteLine("not active updates");
+                Thread.Sleep(3000);
+                update = GetUpdate();
+            }
+
+            int idChat = update.message != null ? update.message.chat.id : update.callback_query.message.chat.id;
+            int idLastUpdate = update.update_id;
+
+            SendMessage(idChat, " ` ", keyboardEmpty);
+
+            Type type = Type.Empty;
+            Word lesson = null;
+
+            while (true)
+            {
+                DateTime now = DateTime.Now;
+                update = GetUpdate();
+
+                if (update.update_id > idLastUpdate)
+                {
+                    // текст
+                    if (update.message != null)
+                    {
+                        switch (update.message.text)
+                        {
+                            case "#":
+                                {
+                                    type = Type.Empty;
+                                    SendMessage(idChat, " ` ", keyboardEmpty);
+                                    
+                                    break;
+                                }
+                            case "#del":
+                                {
+                                    if (type == Type.New)
+                                    {
+                                        words.RemoveAll(w => w.EN == lesson.EN);
+
+                                        lesson = words.OrderBy(w => w.Prev).First();
+                                        SendMessage(idChat, GetWTr(lesson), keyboardNew);
+                                    }
+                                    else
+                                    {
+                                        SendMessage(idChat, " ` the command is not correct ");
+                                    }
+
+                                    break;
+                                }
+                            case "#exit":
+                                {
+                                    File.WriteAllText("word`s", JsonConvert.SerializeObject(words.OrderBy(w => w.EN)));
+                                    SendMessage(idChat, string.Format(" ` write {0} word`s in file ", words.Count));
+
+                                    break;
+                                }
+                            default:
+                                {
+                                    if (type == Type.Add)
+                                    {
+                                        string str = update.message.text.Trim();
+                                        Word equal = words.Find(w => w.EN == str);
+
+                                        if (equal != null)
+                                        {
+                                            SendMessage(idChat, " ` this word already exists ");
+                                            SendMessage(idChat, GetWTr(equal));
+                                        }
+                                        else
+                                        {
+                                            Word word = new Word(str);
+                                            SendMessage(idChat, GetWTr(word));
+                                            words.Add(word);
+                                        }
+                                    }
+
+                                    break;
+                                }
+                        }
+                    }
+
+                    // кнопка
+                    if (update.callback_query != null)
+                    {
+                        switch (update.callback_query.data)
+                        {
+                            case "add":
+                                {
+                                    type = Type.Add;
+                                    SendMessage(idChat, " ` add world`s ");
+                                    
+                                    break;
+                                }
+                            case "new":
+                                {
+                                    if (words.Count(w => w.Next == DateTime.MinValue) > 0)
+                                    {
+                                        type = Type.New;
+                                        SendMessage(idChat, " ` new world`s ");
+
+                                        lesson = words.OrderBy(w => w.Prev).First();
+                                        SendMessage(idChat, GetWTr(lesson), keyboardNew);
+                                    }
+                                    else
+                                    {
+                                        SendMessage(idChat, " ` new world`s not found ", keyboardEmpty);
+                                    }
+
+                                    break;
+                                }
+                            case "tng":
+                                {
+                                    if (words.Count(w => w.Prev != DateTime.MinValue) > 0)
+                                    {
+                                        type = Type.Tng;
+                                        SendMessage(idChat, " ` repetition world`s ");
+
+                                        lesson = words.Where(w => w.Prev != DateTime.MinValue).OrderBy(w => w.Prev).First();
+                                        SendMessage(idChat, GetWEn(lesson), keyboardNew);
+                                    }
+                                    else
+                                    {
+                                        SendMessage(idChat, " ` no word`s found for repetition ", keyboardEmpty);
+                                    }
+
+                                    break;
+                                }
+                            case "save":
+                            case "next":
+                                {
+                                    words.Find(w => w.EN == lesson.EN).Prev = now;
+
+                                    if (update.callback_query.data == "save")
+                                        words.Find(w => w.EN == lesson.EN).Next = now;
+
+                                    lesson = words.OrderBy(w => w.Prev).First();
+                                    SendMessage(idChat, GetWTr(lesson), keyboardNew);
+
+                                    break;
+                                }
+                        }
+                    }
+
+                    idLastUpdate = update.update_id;
+                }
+
+                while ((DateTime.Now - now).TotalSeconds < 1)
+                    Thread.Sleep(250);
+            }
+        }
+
+        static void SendMessage(int idChat, string message)
+        {
+            WebRequest request = WebRequest.Create(string.Format("https://api.telegram.org/bot{0}/sendMessage?chat_id={1}&text={2}", token, idChat, message));
+
+            request.GetResponse();
+            request.Abort();
+        }
+
+        static void SendMessage(int idChat, string message, InlineKeyboardMarkup keyboard)
+        {
+            WebRequest request = WebRequest.Create(string.Format("https://api.telegram.org/bot{0}/sendMessage?chat_id={1}&text={2}&reply_markup={3}", token, idChat, message, JsonConvert.SerializeObject(keyboard)));
+
+            request.GetResponse();
+            request.Abort();
+        }
+
+        static Update GetUpdate()
+        {
+            WebResponse response = WebRequest.Create(string.Format("https://api.telegram.org/bot{0}/getUpdates", token)).GetResponse();
+            StreamReader stream = new StreamReader(response.GetResponseStream());
+
+            Update[] updates = JsonConvert.DeserializeObject<Update[]>(Convert.ToString(JObject.Parse(stream.ReadToEnd())["result"]));
+            stream.Close();
+
+            return updates.Count() > 0 ? updates.Last() : null;
+        }
+
+        static string GetWTr(Word word)
+        {
+            if (word.Ins.Def.Count() > 0)
+                return " ` " + word.EN + " [" + word.Ins.Def.First().Ts + "] - " + word.RU;
+
+            return " ` " + word.EN + " - " + word.RU;
+        }
+
+        static string GetWEn(Word word)
+        {
+            if (word.Ins.Def.Count() > 0)
+                return " ` " + word.EN + " [" + word.Ins.Def.First().Ts + "]";
+
+            return " ` " + word.EN;
+        }
+
+        static InlineKeyboardMarkup GetKeyboard(Word word, Word[] words)
+        {
+            Random rnd = new Random();
+            Word[] others = words.Where(w => w.EN != word.EN).OrderBy(x => rnd.Next()).Take(3).ToArray();
+            Word[] union = others.Union(new Word[] { word }).OrderBy(x => rnd.Next()).ToArray();
+
+            InlineKeyboardMarkup keyboard = new InlineKeyboardMarkup {
+                inline_keyboard = new InlineKeyboardButton[][] {
+                    (new int[] { 0, 1 }).Select(i => new InlineKeyboardButton { text = union[i].RU, callback_data = "#" + union[i].EN }).ToArray(),
+                    (new int[] { 2, 3 }).Select(i => new InlineKeyboardButton { text = union[i].RU, callback_data = "#" + union[i].EN }).ToArray(),
+                }
+            };
+
+            return keyboard;
+        }
+    }
+}
